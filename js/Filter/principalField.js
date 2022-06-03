0a88736b73149614eb54ec55ed5983569c7e0e1e
Agnes <almoha@gmail.com>
2022-06-03 15:18:22 +0200

    commentaires

--- js/Filter/principalField.js
+++ js/Filter/principalField.js
@@ -56,19 +56,22 @@ const buttonUstensilsList = document.querySelector(
     let recipeDescription;
     let recipeIngredients
     const filterValue = e.target.value.trim().toLowerCase();
-    
+    //création nouveau tableau d'objets filtré par nom en fonction de la valeur de l'input
     recipeName = recipes.filter(recipe => recipe.name.toLowerCase().includes(filterValue))
     console.log(recipeName)
-
+    
+    //création nouveau tableau d'objets filtré par description en fonction de la valeur de l'input
     recipeDescription = recipes.filter(recipe => recipe.description.toLowerCase().includes(filterValue))
     console.log(recipeDescription)
 
+    //création nouveau tableau d'objets filtré par ingreédient en fonction de la valeur de l'input
     recipeIngredients = recipes.filter(recipe => recipe.ingredients.find(({ingredient}) =>  ingredient.toLowerCase().includes(filterValue)))
     console.log(recipeIngredients);
     
+    //concaténation des trois tableaux précédents
     const recipeConcat = recipeName.concat(recipeDescription, recipeIngredients)
-    
     console.log(recipeConcat);
+
         if(filterValue.length > 2){
     
            /// fonction remove duplicate objets dans un array
@@ -81,17 +84,18 @@ const buttonUstensilsList = document.querySelector(
                 
             })
             
-            console.log(filteredArr);
+            console.log(filteredArr);// nouveau tableau d'objet trié et sans doublons
 
             //On vide le container des cartes
             container.innerHTML = ''
 
-            //On affiche les nouvelles données triées
+            //On affiche les nouvelles cartes en fonction du nouveau tableau généré par le tri
             filteredArr.forEach((recipe) => {    
                 const recipeDisplay = new Recipe(recipe);
                 recipeDisplay.buildCard();
                 
             });
+            //On affiche les tris dans les boutons en fonction du nouveau tableau généré par le tri
             buttonUstensilsList.innerHTML=''
             const buttonMenuFactory = new ButtonMenuFactory()
                 buttonMenuFactory.workArrayForButton(filteredArr)
